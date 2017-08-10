df431404a03a19157e707ae4b8fdcad91c241568
Esteban Galvis <tebandesade@gmail.com>
2017-08-10 17:35:51 -0400

    .

--- src/extract_pleb.py
+++ src/extract_pleb.py
@@ -4,9 +4,7 @@ from gensim import corpora, models, similarities
 import sys
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-#from pdfrw import PdfReader
 
-#texto = PdfReader(sys.argv[1]) 
 
 pags = [] 
 
@@ -18,12 +16,6 @@ with open(sys.argv[1]) as arch:
 		if not re.match(r'^\s*$', texto):
 			print 'test %%%: ', texto
 			pags.append(texto)
-'''
-for index in range(tam):
-	print len(texto.pages[index].Resources.ProcSet[1])
-	print '\n'
-	print index
-'''
 no_features =1000
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
 tfidf = tfidf_vectorizer.fit_transform(pags)
