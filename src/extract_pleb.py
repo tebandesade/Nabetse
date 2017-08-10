c10584388737dedb457da10e3c749dadcd06b6e4
Esteban Galvis <tebandesade@gmail.com>
2017-08-10 17:41:54 -0400

    dsasdas

--- src/extract_pleb.py
+++ src/extract_pleb.py
@@ -7,14 +7,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 
 pags = [] 
-
+file_ = '../data/acuerdofinal.txt'
 # pagina se dive por acuerdo fina o pagina 
 
-with open(sys.argv[1]) as arch:
+with open(file_) as arch:
 	for line in arch:
 		texto = line.strip()
 		if not re.match(r'^\s*$', texto):
-			print 'test %%%: ', texto
+			print ('test %%%: ', texto)
 			pags.append(texto)
 no_features =1000
 tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2, max_features=no_features, stop_words='english')
@@ -22,4 +22,4 @@ tfidf = tfidf_vectorizer.fit_transform(pags)
 
 tfidf_feature_names = tfidf_vectorizer.get_feature_names()
 
-print tfidf_feature_names
+print (tfidf_feature_names)
